5e05aa38cd6529dd0e73c0ac7ef4e1faf38b5c5c
manel1874 <manel1874@gmail.com>
2023-01-02 11:02:54 +0000

    Common collections: done.

--- the-book/collections-8/src/hashmaps.rs
+++ the-book/collections-8/src/hashmaps.rs
@@ -15,4 +15,38 @@ pub fn working_with_hashmaps() {
         println!("{key}: {value}");
     }
 
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    // Now, at this point we cannot use field_name and field_value because they
+    // are owned by the map hashmap
+    
+    // Overwriting a value
+
+    let mut scores1 = HashMap::new();
+
+    scores1.insert(String::from("Blue"), 10);
+    scores1.insert(String::from("Blue"), 25);
+
+    scores1.entry(String::from("Yellow")).or_insert(50);
+    scores1.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores1);
+
+    // Update based on the old value
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count +=1;
+    }
+
+    println!("{:?}", map);
+
 }
